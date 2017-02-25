86af930c18ce97be64e4cfa1f6ca058035ad91b0
Marco Afonso <mafonso333@gmail.com>
2017-02-25 21:41:33 +0000

    Integrate with datatables

--- assets/admin/js/articles/index.js
+++ assets/admin/js/articles/index.js
@@ -5,10 +5,60 @@ var articles_index = {
 
     data: function () {
         return {
+            headers: [
+                { title: 'ID', data: 'id' },
+                { title: 'Title', data: 'title' },
+                {
+                    data: 'id',
+                    orderable: false,
+                    searchable: false,
+                    render: function (id, a, row) {
+                        return '<a class="btn btn-danger btn-sm pull-right trash">' 
+                            + '<i class="fa fa-trash"></i>' 
+                            + '</a>&nbsp;'
+                            + '<a class="btn btn-success btn-sm pull-right edit">' 
+                            + '<i class="fa fa-pencil"></i>' 
+                            + '</a>';
+                    }
+                }
+            ],
+            dtHandle: null,
             processing: false,
             success: false,
             error: false
         };
+    },
+    
+    watch: {
+        articles: function (rows, old) {
+            var v = this;
+            
+            // Update if already exists
+            if (v.dtHandle) {
+                v.dtHandle.clear();
+                v.dtHandle.rows.add(rows);
+                v.dtHandle.draw();
+                return;
+            }
+            
+            // Create datatables instance
+            v.dtHandle = $(v.$el).find('table').DataTable({
+                columns: v.headers,
+                data: v.articles,
+                initComplete: function () {
+                    
+                    // Init events
+                    $(v.$el).on('click', 'table .edit', function () {
+                        var row = v.dtHandle.row( $(this).parents('tr') ).data();
+                        v.edit(row);
+                    });
+                    $(v.$el).on('click', 'table .trash', function () {
+                        var row = v.dtHandle.row( $(this).parents('tr') ).data();
+                        v.del(row);
+                    });
+                }
+            });
+        }
     },
 
     methods: {
--- assets/admin/js/users/index.js
+++ assets/admin/js/users/index.js
@@ -5,10 +5,60 @@ var users_index = {
     props:['users'],
     data: function () {
         return {
+            headers: [
+                { title: 'ID', data: 'id' },
+                { title: 'Username', data: 'username', class: 'hidden-xs' },
+                { title: 'Email', data: 'email' },
+                {
+                    data: 'id',
+                    orderable: false,
+                    searchable: false,
+                    render: function (id, a, row) {
+                        return '<a class="btn btn-danger btn-sm pull-right trash">' 
+                            + '<i class="fa fa-trash"></i>' 
+                            + '</a>&nbsp;'
+                            + '<a class="btn btn-success btn-sm pull-right edit">' 
+                            + '<i class="fa fa-pencil"></i>' 
+                            + '</a>';
+                    }
+                }
+            ],
+            dtHandle: null,
             processing: false,
             success: false,
             error: false
         };
+    },
+    watch: {
+        users: function (rows, old) {
+            var v = this;
+            
+            // Update if already exists
+            if (v.dtHandle) {
+                v.dtHandle.clear();
+                v.dtHandle.rows.add(rows);
+                v.dtHandle.draw();
+                return;
+            }
+            
+            // Create datatables instance
+            v.dtHandle = $(v.$el).find('table').DataTable({
+                columns: v.headers,
+                data: v.users,
+                initComplete: function () {
+                    
+                    // Init events
+                    $(v.$el).on('click', 'table .edit', function () {
+                        var user = v.dtHandle.row( $(this).parents('tr') ).data();
+                        v.edit(user);
+                    });
+                    $(v.$el).on('click', 'table .trash', function () {
+                        var user = v.dtHandle.row( $(this).parents('tr') ).data();
+                        v.del(user);
+                    });
+                }
+            });
+        }
     },
     methods: {
         formNew: function () {
